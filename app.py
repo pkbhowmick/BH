96bd98ebf1b7a4e29527c8eb0c910bce7ca370c7
pkbhowmick <pkbhowmick007@gmail.com>
2020-03-19 16:39:01 +0600

    second commit

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import json
 import sys
 import os
 from flask_sqlalchemy import SQLAlchemy
-from models import *
 app = Flask(__name__)
 
 def get_env_variable(name):
@@ -18,7 +17,7 @@ POSTGRES_URL = get_env_variable("POSTGRES_URL")
 #POSTGRES_USER = get_env_variable("POSTGRES_USER")
 POSTGRES_USER = "postgres"
 #POSTGRES_PW = get_env_variable("POSTGRES_PW")
-POSTGRES_PW = "dbpw" 
+POSTGRES_PW = "zxcvbnm" 
 #POSTGRES_DB = get_env_variable("POSTGRES_DB")
 POSTGRES_DB = "test"
 
@@ -29,23 +28,57 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # silence the deprecation w
 
 db = SQLAlchemy(app)
 
+class User(db.Model):
+
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    contact = db.Column(db.String(30))
+    institution = db.Column(db.String(50))
+    designation = db.Column(db.String(30))
+
+    def __init__(self, name, email, contact, institution, designation):
+        self.name = name
+        self.email = email
+        self.contact = contact
+        self.institution = institution
+        self.designation = designation
+    
 @app.route('/')
 def signup():
         return render_template('signup.html')
 
 @app.route('/register', methods=['GET','POST'])
-def register_guest():
-    name = request.form.get('name')
-    email = request.form.get('email')
-    contact = request.form.get('contact')
-    institution = request.form.get('institution')
-    designation = request.form.get('designation')
+def register_user():
+    name = request.args.get('name')
+    email = request.args.get('email')
+    contact = request.args.get('contact')
+    institution = request.args.get('institution')
+    designation = request.args.get('designation')
 
-    user = User(name, email,contact,institution,designation)
+    user = User(name,email,contact,institution,designation)
     db.session.add(user)
     db.session.commit()
 
-    return render_template('confirm.html', name=name, email=email)
+    return render_template('confirm.html',name= name , email= email)
+
+@app.cli.command('resetdb')
+def resetdb_command():
+    """Destroys and creates the database + tables."""
+
+    from sqlalchemy_utils import database_exists, create_database, drop_database
+    if database_exists(DB_URL):
+        print('Deleting database.')
+        drop_database(DB_URL)
+    if not database_exists(DB_URL):
+        print('Creating database.')
+        create_database(DB_URL)
+
+    print('Creating tables.')
+    db.create_all()
+    print('Shiny!')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from app import db
+db = SQLAlchemy()
 
 class User(db.Model):
 
